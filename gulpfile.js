5fbd626df98adc0af6a76d4c069ceb02c53e1d80
Thomas <truethomaslieben@gmail.com>
2017-02-09 16:26:50 +0100

    Gulp task for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require ( "gulp" ),
-    image = require( "gulp-image" );
+    image = require( "gulp-image" ),
+    sass = require( "gulp-sass" ),
+    autoprefixer = require( "gulp-autoprefixer" ),
+    cssp = require( "gulp-csso" );
 
 // --- Task for images
+
 gulp.task( "images", function() {
     gulp.src( "src/images/**" )
         .pipe( image() )
         .pipe( gulp.dest( "assets/images" ) );
-});
+} );
+
 // --- Task for styles
+
+gulp.task( "css", function() {
+    gulp.src( "src/sass/**/*.scss" )
+        .pipe( sass().on( "error", sass.logError ) )
+        .pipe( autoprefixer() )
+        .pipe( csso() )
+        .pipe( gulp.dest( "assets/css" ) );
+} );
 
 // --- Task for pug
 
